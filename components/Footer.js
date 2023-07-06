43714c3cb82fac8e08cb01c9fcc298742f0c8954
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-06 10:46:08 +0530

    minor UI changes

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Button from './Button'
 import Link from 'next/link';
 import Logo from './../public/Cine.svg'
+import { AiFillFacebook,AiFillInstagram,AiFillLinkedin,AiFillTwitterCircle } from "react-icons/ai";
 
 const Footer = () => {
     return (
@@ -13,29 +14,35 @@ const Footer = () => {
                     <Logo/>
                 </div>
             </div>
-            <div className='flex justify-center md:justify-between flex-wrap'>
-                <div className="py-5 md:p-5 text-[#aaa]  flex flex-col gap-3 mt-8">
+            <div className='flex justify-start md:justify-between flex-wrap'>
+                <div className="py-5 md:py-5 text-[#aaa]  flex flex-col gap-10">
+                    <div className=' flex justify-start content-center gap-6 md-[20px]'>
+                        <a href='#'><AiFillLinkedin className='w-[25px] h-[25px] '/></a>
+                        <a href='#'><AiFillInstagram className='w-[25px] h-[25px] '/></a>
+                        <a href='#'><AiFillFacebook className='w-[25px] h-[25px] '/></a>
+                        <a href='#'><AiFillTwitterCircle className='w-[25px] h-[25px] '/></a>
+                    </div>
                     &#169; CineWealth. All rights reserved. Built in India
                 </div>
                 <div className='text-[#aaa] flex gap-[80px] justify-start md:justify-center items-start md:items-start flex-wrap mt-2 '>
                     <div className='text-md font-extrabold flex flex-col gap-3 cursor-pointer'>
-                        <div className='mb-5'>FOR INVESTORS</div>
-                        <Link href={"#ff"} className='text-white font-bold hover:underline'>Home</Link>
-                        <Link href={"#offer"}  className='text-white font-bold hover:underline'>Offerings</Link>
-                        <Link href={"#faqs"} className='text-white font-bold hover:underline'>Cardano</Link>
-                        <Link href={"#faqs"} className='text-white font-bold hover:underline'>FAQs</Link>
+                        <div className='mb-5 '>FOR INVESTORS</div>
+                        <Link href={"#ff"} className='text-white text-[14px] font-bold hover:underline'>Home</Link>
+                        <Link href={"#offer"}  className='text-white text-[14px] font-bold hover:underline'>Offerings</Link>
+                        <Link href={"#faqs"} className='text-white text-[14px] font-bold hover:underline'>Cardano</Link>
+                        <Link href={"#faqs"} className='text-white text-[14px] font-bold hover:underline'>FAQs</Link>
                         {/* <div className='text-white font-bold hover:underline'>Blog</div>
                         <div className='text-white font-bold hover:underline'>Risk disclosure</div> */}
                     </div>
                     <div className='text-md font-extrabold flex flex-col gap-3 cursor-pointer'>
                         <div className='mb-5'>COMPANY</div>
-                        <div className='text-white font-bold hover:underline'>About Us</div>
-                        <div className='text-white font-bold hover:underline'>Contact us</div>
+                        <div className='text-white text-[14px] font-bold hover:underline'>About Us</div>
+                        <div className='text-white text-[14px] font-bold hover:underline'>Contact us</div>
                     </div>
                     <div className='text-md font-extrabold flex flex-col gap-3 cursor-pointer'>
                         <div className='mb-5'>LEGAL</div>
-                        <div className='text-white font-bold hover:underline'>Terms of use</div>
-                        <div className='text-white font-bold hover:underline'>Privacy policy</div>
+                        <div className='text-white text-[14px] font-bold hover:underline'>Terms of use</div>
+                        <div className='text-white text-[14px] font-bold hover:underline'>Privacy policy</div>
                     </div>
                 </div>
             </div>
